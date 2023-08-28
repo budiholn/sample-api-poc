88b4d27e6b8544de1063403410be91a192274829
budiholn <112673378+budiholn@users.noreply.github.com>
2023-08-28 09:15:58 +0700

    Update delete-old-image-registry.sh

--- delete-old-image-registry.sh
+++ delete-old-image-registry.sh
@@ -1,6 +1,9 @@
 ssh rancher@rnd-node4
 sudo docker rmi registry.hub.docker.com/budiholan/jenkins-api-poc:<TheTag>
+logout
 ssh rancher@rnd-node5
 sudo docker rmi registry.hub.docker.com/budiholan/jenkins-api-poc:<TheTag>
+logout
 ssh rancher@rnd-node6
 sudo docker rmi registry.hub.docker.com/budiholan/jenkins-api-poc:<TheTag>
+logout
