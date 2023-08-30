b832fd3a4e4c7ca92f2f338a683748d01abaf021
budiholn <112673378+budiholn@users.noreply.github.com>
2023-08-30 16:31:56 +0700

    Create del-image-hub-docker.sh

---
+++ del-image-hub-docker.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+USERNAME="budiholan"
+PASSWORD="dckr_pat_n5vTR_ZVXjze7JB_yyXgZsYM6SU"
+ORGANIZATION="budiholan"
+IMAGE="jenkins-api-poc"
+TAG="145"
+
+login_data() {
+cat <<EOF
+{
+  "username": "$USERNAME",
+  "password": "$PASSWORD"
+}
+EOF
+}
+
+TOKEN=`curl -s -H "Content-Type: application/json" -X POST -d "$(login_data)" "https://hub.docker.com/v2/users/login/" | jq -r .token`
+
+curl "https://hub.docker.com/v2/repositories/${ORGANIZATION}/${IMAGE}/tags/${TAG}/" -X DELETE -H "Authorization: JWT ${TOKEN}" || true
